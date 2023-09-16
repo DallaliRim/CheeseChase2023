62efc8b1a0ba523fa1b4e2cd74ae44f91ccdcb6f
NAD - GameJam <gamejam@LAB>
2023-09-16 11:54:35 -0400

    implement movement for blue player

--- Assets/Scripts/Characters/Player/GridMovementRed.cs
+++ Assets/Scripts/Characters/Player/GridMovementRed.cs
@@ -1,57 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GridMovementRed : MonoBehaviour
 {
-
+    public LayerMask layerMask;
     public float gridSize;
 
     // Start is called before the first frame update
     void Start()
     {
-        //test
     }
-
-    public LayerMask layerMask;
-
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) && !HitRaycast(Vector2.up))
+        if (Input.GetKeyDown(KeyCode.W) && !HitRaycast(Vector2.up))
         {
             transform.Translate(Vector2.up * gridSize);
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow) && !HitRaycast(Vector2.down))
+        if (Input.GetKeyDown(KeyCode.S) && !HitRaycast(Vector2.down))
         {
             transform.Translate(Vector2.down * gridSize);
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow) && !HitRaycast(Vector2.right))
+        if (Input.GetKeyDown(KeyCode.D) && !HitRaycast(Vector2.right))
         {
             transform.Translate(Vector2.right * gridSize);
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && !HitRaycast(Vector2.left))
+        if (Input.GetKeyDown(KeyCode.A) && !HitRaycast(Vector2.left))
         {
             transform.Translate(Vector2.left * gridSize);
         }
 
     }
 
-    // private void FixedUpdate()
-    // {
-    //     HitRaycast(Vector2.up);
-    // }
-
     bool HitRaycast(Vector2 fwd)
     {
-
-        //Length of the ray
-        float laserLength = 1f;
+        float laserLength = 1f; //Length of the ray
 
         //Get the first object hit by the ray
-
         RaycastHit2D hit = Physics2D.Raycast(transform.position, fwd, laserLength, ~layerMask);
+        
         //Method to draw the ray in scene for debug purpose
         Debug.DrawRay(transform.position, fwd * laserLength, Color.red);
         if (hit.collider != null)
@@ -65,7 +52,5 @@ public class GridMovementRed : MonoBehaviour
             Debug.Log("Hiting nothing");
             return false;
         }
-
-
     }
 }
