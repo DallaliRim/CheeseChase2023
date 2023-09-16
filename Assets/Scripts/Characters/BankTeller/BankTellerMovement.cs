0e9a962958c8c5ba9c89152a4263c0e0dc243b6f
NAD - GameJam <gamejam@LAB>
2023-09-16 17:36:41 -0400

    Fixed bank tellers

--- Assets/Scripts/Characters/BankTeller/BankTellerMovement.cs
+++ Assets/Scripts/Characters/BankTeller/BankTellerMovement.cs
@@ -1,30 +1,31 @@
 using UnityEngine;
 
 public class BankTellerMovement : MonoBehaviour
-{public float moveSpeed = 2000.0f;
-    public float maxDistance = 1.0f; 
+{
+    public float moveSpeed = 2000.0f;
+    public float maxDistance = 1.0f;
 
     private Vector2 startingPosition;
     private bool centered;
 
     private void Start()
     {
-        startingPosition = transform.position; 
+        startingPosition = transform.position;
     }
 
-    private void FixedUpdate()
+    public void Move()
     {
-        if(centered)
+        if (centered)
         {
-            Vector2 randomDirection = Vector2.zero; 
+            Vector2 randomDirection = Vector2.zero;
 
             if (Random.value < 0.5f)
             {
-                randomDirection.x = Random.Range(-1f, 1f); 
+                randomDirection.x = Random.Range(-1f, 1f);
             }
             else
             {
-                randomDirection.y = Random.Range(-1f, 1f); 
+                randomDirection.y = Random.Range(-1f, 1f);
             }
 
             Vector2 newPosition = (Vector2)transform.position + randomDirection.normalized * moveSpeed * Time.deltaTime;
@@ -32,7 +33,9 @@ public class BankTellerMovement : MonoBehaviour
 
             transform.position = clampedPosition;
             centered = false;
-        } else {
+        }
+        else
+        {
             transform.position = startingPosition;
             centered = true;
         }
