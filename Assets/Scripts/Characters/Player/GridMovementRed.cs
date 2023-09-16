2e7d866b059f1904a368f1646e3e4f8b7e267da9
inxendere <taief.ahmed04@gmail.com>
2023-09-16 11:22:47 -0400

    van wip and npc collision done

--- Assets/Scripts/Characters/Player/GridMovementRed.cs
+++ Assets/Scripts/Characters/Player/GridMovementRed.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GridMovementRed : MonoBehaviour
 {
+
     public float gridSize;
 
     // Start is called before the first frame update
@@ -12,25 +13,59 @@ public class GridMovementRed : MonoBehaviour
         //test
     }
 
+    public LayerMask layerMask;
+
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && !HitRaycast(Vector2.up))
         {
             transform.Translate(Vector2.up * gridSize);
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow) && !HitRaycast(Vector2.down))
         {
             transform.Translate(Vector2.down * gridSize);
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow) && !HitRaycast(Vector2.right))
         {
             transform.Translate(Vector2.right * gridSize);
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && !HitRaycast(Vector2.left))
         {
             transform.Translate(Vector2.left * gridSize);
         }
+
+    }
+
+    // private void FixedUpdate()
+    // {
+    //     HitRaycast(Vector2.up);
+    // }
+
+    bool HitRaycast(Vector2 fwd)
+    {
+
+        //Length of the ray
+        float laserLength = 1f;
+
+        //Get the first object hit by the ray
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, fwd, laserLength, ~layerMask);
+        //Method to draw the ray in scene for debug purpose
+        Debug.DrawRay(transform.position, fwd * laserLength, Color.red);
+        if (hit.collider != null)
+        {
+            //Hit something, print the tag of the object
+            Debug.Log("Hitting: " + hit.collider.gameObject.name);
+            return true;
+        }
+        else
+        {
+            Debug.Log("Hiting nothing");
+            return false;
+        }
+
 
     }
 }
