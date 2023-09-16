7267817d04ee5d07c7e862c36c6a225eb881a811
NAD - GameJam <gamejam@LAB>
2023-09-16 18:29:49 -0400

    get count down ish

--- Assets/Scripts/MainMenu/CountDown.cs
+++ Assets/Scripts/MainMenu/CountDown.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CountdownController : MonoBehaviour
 {
-    public Text countdownText;
+    public TextMeshProUGUI countdownText;
     public float countdownDuration = 1.0f;
 
     private void Start()
@@ -28,5 +29,6 @@ public class CountdownController : MonoBehaviour
         yield return new WaitForSeconds(countdownDuration);
 
         countdownText.gameObject.SetActive(false); // Hide the countdown text
+        Time.timeScale = 1.0f;
     }
 }
