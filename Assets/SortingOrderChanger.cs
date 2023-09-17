4dbd410c6b45204445239152c7143e064b4419eb
inxendere <taief.ahmed04@gmail.com>
2023-09-17 15:48:43 -0400

    collisiond done

--- Assets/SortingOrderChanger.cs
+++ Assets/SortingOrderChanger.cs
@@ -1,8 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class SortingOrderChanger : MonoBehaviour
 {
+    public SpriteRenderer spriteRenderer;
+
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     
+
 }
