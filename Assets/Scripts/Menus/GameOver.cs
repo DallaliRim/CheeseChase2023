f300b4e5bbd7894f84843d910efd113ad93279ba
NAD - GameJam <gamejam@LAB>
2023-09-17 13:30:08 -0400

    add quit and restart in game over

--- Assets/Scripts/Menus/GameOver.cs
+++ Assets/Scripts/Menus/GameOver.cs
@@ -2,25 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameOver : MonoBehaviour
 {
     public TextMeshProUGUI redPlayerText;
     public TextMeshProUGUI bluePlayerText;
+    public TextMeshProUGUI result;
+    public TextMeshProUGUI gameover;
     public Manager_Money moneyManager;
-    public void setup(int score)
+    public void setup(bool win)
     {
         gameObject.SetActive(true);
-        redPlayerText.text = $"Red Thief: {moneyManager.moneyPlayerRed} $$";
-        bluePlayerText.text = $"Blue Thief: {moneyManager.moneyPlayerBlue} $$";
-    }
-    void Start()
-    {
-        
+        redPlayerText.text = win ? $"Red Thief: {moneyManager.moneyPlayerRed} $$" : "";
+        bluePlayerText.text = win ? $"Blue Thief: {moneyManager.moneyPlayerBlue} $$" : "";
+        result.text = win ? "You Escaped Successfully!" : "You Got Caught. ):";
+        gameover.text = win ? "HEIST COMPLETE" : "GAME OVER";
     }
 
-    void Update()
+    public void Restart()
     {
-        
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Quit() 
+    {
+        Application.Quit();
     }
 }
